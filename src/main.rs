81f8cb4a5a76f60b695af3459782d13f4381331c
ndgndg91 <ndgndg91@gmail.com>
2021-02-07 16:10:30 +0900

    <feat> tuple

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,18 @@ fn main() {
 
     grocery_item();
     beverage();
+
+    let coord = (2,3);
+    println!("{:?}, {:?}", coord.0, coord.1);
+
+    let (x, y) = (2,3);
+    println!("{:?}, {:?}", x, y);
+
+    let user_info = ("Dong-Gil", 31);
+    println!("{:?}, {:?}", user_info.0, user_info.1);
+
+    let favorites = ("Game", "Dev", "rust", "java", "go", "spring", "js", "typescript");
+    println!("{:?}, {:?}, {:?} {:?}", favorites.0, favorites.2, favorites.4, favorites.7);
 }
 
 enum Direction {
