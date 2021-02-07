cc2dc37a8505941dbbffd39d9d85a05811eab97f
ndgndg91 <ndgndg91@gmail.com>
2021-02-07 14:36:26 +0900

    <feat> println, add function

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,16 @@ fn main() {
 
     let mut writer = BufWriter::new(stdout());
     say(out, width, &mut writer).unwrap();
+
+    let result_1 = add(1, 9);
+    let result_2 = add(10, 90);
+    let result_3 = add(result_2, 900);
+
+    println!("1 + 9 = {:?}", result_1);
+    println!("10 + 90 = {:?}", result_2);
+    println!("result_2 + 900 = {:?}", result_3);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
