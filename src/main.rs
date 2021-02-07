362094e3cb5f6e4c6540e79707b39b1558ddcff8
ndgndg91 <ndgndg91@gmail.com>
2021-02-07 15:45:20 +0900

    <feat> enum with match

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,28 @@ fn main() {
 
     just_match_integer();
     just_match_string();
+
+    let go = Direction::UP;
+    which_way(go);
+    which_way(Direction::DOWN);
+    which_way(Direction::LEFT);
+    which_way(Direction::RIGHT);
+}
+
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+fn which_way(go: Direction) {
+    match go {
+        Direction::UP => println!("go up"),
+        Direction::DOWN => println!("go down"),
+        Direction::LEFT => println!("go left"),
+        Direction::RIGHT => println!("go right"),
+    }
 }
 
 fn add(a: i32, b: i32) -> i32 {
