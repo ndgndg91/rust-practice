00172c7241fcd4b13b58f96a83d1e571b8f21472
ndgndg91 <ndgndg91@gmail.com>
2021-02-12 14:46:05 +0900

    <feat> Option with Grocery Store

--- src/builtin/grocery_item.rs
+++ src/builtin/grocery_item.rs
@@ -1,12 +1,46 @@
+struct GroceryStore {
+    grocery_items: Vec<GroceryItem>,
+}
+
+#[allow(dead_code)]
 struct GroceryItem {
+    name: String,
     stock: i32,
     price: f64,
 }
 
+impl GroceryStore {
+    fn find_stock_by_name(&self, name: &str) -> Option<i32> {
+        for item in &self.grocery_items {
+            if item.name == name {
+                return Some(item.stock);
+            }
+        }
+        None
+    }
+}
+
 pub fn practice() {
-    let item = GroceryItem {
-        stock: 10,
-        price: 9500.0,
-    };
-    println!("price : {:?}, stock : {:?}", item.price, item.stock);
+    let store = GroceryStore{ grocery_items: vec![
+        GroceryItem {
+            name: "Banana-Milk".to_owned(),
+            stock: 10,
+            price: 2500.0,
+        },
+        GroceryItem{
+            name: "Merona".to_owned(),
+            stock: 30,
+            price: 1500.0
+        },
+        GroceryItem{
+            name: "My-Gumi".to_owned(),
+            stock: 15,
+            price: 1000.0
+        }
+    ]};
+    
+    let merona_stock = store.find_stock_by_name("Merona");
+    println!("Merona stock : {:?}", merona_stock);
+    let none = store.find_stock_by_name("not-exists");
+    println!("none : {:?}", none);
 }
